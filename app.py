60f22a76c3744b44a0ec169ea409e4375a897f39
Arjuna3667 <mallikarjuna.maddu@gmail.com>
2026-05-31 09:10:15 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,25 +2,47 @@
 import gradio as gr
 import tensorflow as tf
 import numpy as np
-from PIL import Image
+import cv2
 
-model = tf.keras.models.load_model("best_pneumonia_model.h5")
-IMG_SIZE=(224,224)
-classes=["Normal","Pneumonia"]
+# Load model
+model = tf.keras.models.load_model(
+    'best_pneumonia_model.h5'
+)
 
 def predict(img):
-    img = img.convert("RGB").resize(IMG_SIZE)
-    arr = np.array(img)/255.0
-    arr = np.expand_dims(arr,0)
-    pred = model.predict(arr)[0][0]
-    label = classes[int(pred>0.5)]
-    prob = float(pred if pred>0.5 else 1-pred)
-    return {"Predicted Class": label, "Probability": prob}
+
+    # Resize
+    img = cv2.resize(img, (224,224))
+
+    # Normalize
+    img = img / 255.0
+
+    # Convert grayscale → RGB
+    if len(img.shape)==2:
+        img = cv2.cvtColor(
+            img,
+            cv2.COLOR_GRAY2RGB
+        )
+
+    img = np.expand_dims(img, axis=0)
+
+    pred = model.predict(img)[0][0]
+
+    label = (
+        "Pneumonia"
+        if pred > 0.5
+        else "Normal"
+    )
+
+    return {
+        label: float(pred)
+    }
 
 demo = gr.Interface(
     fn=predict,
-    inputs=gr.Image(type="pil"),
-    outputs="label",
-    title="Pneumonia Detection"
+    inputs=gr.Image(),
+    outputs=gr.Label(),
+    title="Pneumonia Detection App"
 )
+
 demo.launch()
