d9d8aa6e91d15abd7423e51224b96212a46f5f53
Arjuna3667 <mallikarjuna.maddu@gmail.com>
2026-05-31 09:22:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-
 import gradio as gr
 import tensorflow as tf
 import numpy as np
 import cv2
+import pydicom
 
 # Load model
 model = tf.keras.models.load_model(
-    'best_pneumonia_model.h5'
+    "best_pneumonia_model.h5"
 )
 
-def predict(img):
+def predict(file):
+
+    # Read DICOM
+    ds = pydicom.dcmread(file.name)
+    img = ds.pixel_array
 
     # Resize
     img = cv2.resize(img, (224,224))
@@ -18,31 +22,34 @@ def predict(img):
     img = img / 255.0
 
     # Convert grayscale → RGB
-    if len(img.shape)==2:
-        img = cv2.cvtColor(
-            img,
-            cv2.COLOR_GRAY2RGB
-        )
+    img = cv2.cvtColor(
+        img.astype(np.float32),
+        cv2.COLOR_GRAY2RGB
+    )
 
     img = np.expand_dims(img, axis=0)
 
     pred = model.predict(img)[0][0]
 
-    label = (
-        "Pneumonia"
-        if pred > 0.5
-        else "Normal"
-    )
+    pneumonia_prob = float(pred)
+    normal_prob = 1 - pneumonia_prob
 
     return {
-        label: float(pred)
+        "Pneumonia": pneumonia_prob,
+        "Normal": normal_prob
     }
 
 demo = gr.Interface(
     fn=predict,
-    inputs=gr.Image(),
-    outputs=gr.Label(),
-    title="Pneumonia Detection App"
+    inputs=gr.File(
+        file_types=[".dcm"],
+        label="Upload DICOM Chest X-ray (.dcm)"
+    ),
+    outputs=gr.Label(
+        num_top_classes=2
+    ),
+    title="Pneumonia Detection from DICOM X-ray",
+    description="Upload a DICOM (.dcm) chest X-ray image."
 )
 
 demo.launch()
